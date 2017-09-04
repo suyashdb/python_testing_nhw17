1d2939fd6de10e90402952f4bb98e95ca9f8b26c
Suyash <suyashdb@gmail.com>
2017-09-04 14:54:19 -0700

    assert

--- demean_age.py
+++ demean_age.py
@@ -1,5 +1,7 @@
 import numpy as np
 age = np.loadtxt("participants.tsv", skiprows=1, usecols=3)
 mean_age = sum(age)/len(age)
+assert mean_age < 120
+assert mean_age > 10;
 np.savetxt("demeaned_age.txt", age-mean_age)
 print("done!!")
